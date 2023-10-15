60077f24bd1a280f301523768402d7075e2f229e
hidelgionovela <hidelgionovela@gmail.com>
2023-10-15 23:22:45 +0200

    chore: create userRepository

---
+++ src/main/java/com/hdlg/todolist/user/UserRepository.java
@@ -0,0 +1,12 @@
+package com.hdlg.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<UserModel, UUID>{
+
+         UserModel findByUsername(String username);
+         
+}
