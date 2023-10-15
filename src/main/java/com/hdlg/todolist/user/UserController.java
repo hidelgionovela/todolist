6dafff6386f4682dc2c0792c0e593a2c6681722e
hidelgionovela <hidelgionovela@gmail.com>
2023-10-15 23:21:35 +0200

    refactor: updated userController

--- src/main/java/com/hdlg/todolist/user/UserController.java
+++ src/main/java/com/hdlg/todolist/user/UserController.java
@@ -1,18 +1,37 @@
 package com.hdlg.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-         @PostMapping
-         public void create(@RequestBody UserModel userModel) {
+         @Autowired
+         private UserRepository userRepository;
 
-                  System.out.println("nome: "+userModel.username);
+         @PostMapping
+         public ResponseEntity create(@RequestBody UserModel userModel) {
+                  var user = this.userRepository.findByUsername(userModel.getUsername());
+
+                  if (user != null) {
+                           System.out.println("Ja existe um user com o mesmo user name");
+                           return ResponseEntity.status(400).body("Ja existe um user com o mesmo user name");
+                  }
+
+                  String passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+                  userModel.setPassword(passwordHashed);
+
+                  var userCreated = this.userRepository.save(userModel);
+                  System.out.println("nome: "+userModel.getUsername());
+                  return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
 
          }
          
